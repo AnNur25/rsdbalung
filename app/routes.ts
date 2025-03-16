d592f53705712c8cefa85a40058159cbb9981778
ivantrywicaksono <ivantrywicaksono04@gmail.com>
2025-03-17 03:32:04 +0700

    temp fs-routes config

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,14 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
 export default [index("routes/home.tsx")] satisfies RouteConfig;
+
+// npm i @react-router/fs-routes
+
+// import { type RouteConfig, route } from '@react-router/dev/routes';
+// import { flatRoutes } from '@react-router/fs-routes';
+
+// export default [
+//   route('/', './home.tsx'),
+
+//   ...(await flatRoutes()),
+// ] satisfies RouteConfig;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Rumah Sakit Daerah Balung" },
+    { name: "description", content: "Selamat Datang Di Website Rumah Sakit Daerah Balung!" },
   ];
 }
 
