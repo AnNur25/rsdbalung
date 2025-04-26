3c1db59822f4cef3aca53e332e44b6d9444479b1
ivantrywicaksono <ivantrywicaksono04@gmail.com>
2025-04-26 14:38:24 +0700

    sanitize fix

--- app/components/HtmlParse.tsx
+++ app/components/HtmlParse.tsx
@@ -1,7 +1,13 @@
 import DOMPurify from "dompurify";
+import { useEffect, useState } from "react";
 
 export default function HtmlParse({ htmlString }: { htmlString: string }) {
-  const sanitizedHtml: string = DOMPurify.sanitize(htmlString);
+  const [sanitizedHtml, setSanitizedHtml] = useState<string>("");
+
+  useEffect(() => {
+    const clean = DOMPurify.sanitize(htmlString);
+    setSanitizedHtml(clean);
+  }, [htmlString]);
 
   return <div dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />;
 }
