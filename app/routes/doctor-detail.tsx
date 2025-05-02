4639012b1e6be665619f464b911383fb1dbca1b0
ivantrywicaksono <ivantrywicaksono04@gmail.com>
2025-05-02 15:01:40 +0700

    doctors slider in detail doctor

--- app/routes/doctor-detail.tsx
+++ app/routes/doctor-detail.tsx
@@ -9,6 +9,8 @@ import InstagramIcon from "~/components/InstagramIcon";
 import imageErrorHandler from "~/utils/imageErrorHandler";
 import Table from "~/components/Table";
 import DoctorCard from "~/components/DoctorCard";
+import { alternatingRowColor } from "~/utils/styles";
+import Slider from "~/components/Slider";
 
 export async function loader({ params }: Route.LoaderArgs) {
   const doctorRequest = new URL(
@@ -17,7 +19,7 @@ export async function loader({ params }: Route.LoaderArgs) {
   const doctorResponse = await handleLoader(() =>
     axios.get(doctorRequest.href),
   );
-  const allDoctorRequest = new URL(`https://rs-balung-cp.vercel.app/dokter/`);
+  const allDoctorRequest = new URL(`https://rs-balung-cp.vercel.app/dokter?pageSize=8`);
   const allDoctorResponse = await handleLoader(() =>
     axios.get(allDoctorRequest.href),
   );
@@ -43,7 +45,7 @@ export async function loader({ params }: Route.LoaderArgs) {
 
 export default function DoctorDetail({ loaderData }: Route.ComponentProps) {
   const headers = ["No", "Dokter", "Layanan", "Hari", "Jam", "Sesi"];
-  const doctor: Doctor = loaderData.data.doctor;
+  const schedule: Doctor = loaderData.data.doctor;
   const doctors: Doctor[] = loaderData.data.doctors || [];
   const schedules: DokterSchedule = loaderData.data.schedules || {};
 
@@ -61,20 +63,27 @@ export default function DoctorDetail({ loaderData }: Route.ComponentProps) {
   return (
     <>
       <div className="flex flex-col">
-        <div className="flex flex-col items-center justify-center gap-8 p-8 min-md:flex-row">
+        <div className="flex flex-col  justify-center gap-8 p-8 min-md:flex-row">
           <div className="flex w-fit flex-col items-center">
             <img
               onError={imageErrorHandler}
               className="aspect-[9/12] h-auto w-full rounded-sm object-cover min-md:w-64"
-              src={doctor.gambar}
-              alt={`Foto ${doctor.nama}`}
+              src={schedule.gambar}
+              alt={`Foto ${schedule.nama}`}
             />
             <div className="flex w-fit flex-col items-center">
               <p className="font-semibold text-gray-700">Profil Dokter</p>
               <div className="flex">
-                <LinkedinIcon iconSize={42} className={"text-gray-500 hover:text-blue-900"} />
-                <FacebookIcon iconSize={42} className={"text-gray-500 hover:text-blue-900"} />
-                <InstagramIcon iconSize={42}
+                <LinkedinIcon
+                  iconSize={42}
+                  className={"text-gray-500 hover:text-blue-900"}
+                />
+                <FacebookIcon
+                  iconSize={42}
+                  className={"text-gray-500 hover:text-blue-900"}
+                />
+                <InstagramIcon
+                  iconSize={42}
                   className={"text-gray-500 hover:text-blue-900"}
                 />
               </div>
@@ -83,15 +92,15 @@ export default function DoctorDetail({ loaderData }: Route.ComponentProps) {
 
           <div className="flex flex-col">
             <h1 className="bg-gradient-to-l from-blue-300 to-blue-950 bg-clip-text text-justify text-2xl font-bold text-transparent">
-              {doctor.nama}
+              {schedule.nama}
             </h1>
             <p className="bg-gradient-to-l from-blue-300 to-blue-950 bg-clip-text text-lg font-semibold text-transparent uppercase">
-              {doctor.poli.nama_poli}
+              {schedule.poli.nama_poli}
             </p>
             <hr className="border- my-4 w-full border-dashed border-gray-700" />
             <p>Biodata Singkat</p>
             <p className="text-justify text-gray-700">
-              {doctor.biodata_singkat}
+              {schedule.biodata_singkat}
             </p>
             <hr className="border- my-4 w-full border-dashed border-gray-700" />
             <p>Jadwal Praktek</p>
@@ -100,46 +109,48 @@ export default function DoctorDetail({ loaderData }: Route.ComponentProps) {
             </p>
             <section className="max-w-[90vw] overflow-auto">
               <Table headers={headers}>
-                {/* {flattenedSchedules.jadwal.map((jadwal, jIndex) => (
-            <tr key={index} className={alternatingRowColor}>
-            {jIndex === 0 && (
-                <>
-                <td
-                rowSpan={doctor.jadwal.length}
-                className="w-min border border-gray-300 px-4 py-2 text-center"
-                >
-                {index + 1}
-                </td>
-                <td
-                rowSpan={doctor.jadwal.length}
-                className="border border-gray-300 px-4 py-2"
-                >
-                {doctor.dokter}
-                </td>
-                <td
-                rowSpan={doctor.jadwal.length}
-                className="border border-gray-300 px-4 py-2"
-                >
-                {doctor.poli}
-                </td>
-                </>
+                {flattenedSchedules.map((schedule, index) =>
+                  schedule.jadwal.map((j, jIndex) => (
+                    <tr key={index} className={alternatingRowColor}>
+                      {jIndex === 0 && (
+                        <>
+                          <td
+                            rowSpan={schedule.jadwal.length}
+                            className="w-min border border-gray-300 px-4 py-2 text-center"
+                          >
+                            {index + 1}
+                          </td>
+                          <td
+                            rowSpan={schedule.jadwal.length}
+                            className="border border-gray-300 px-4 py-2"
+                          >
+                            {schedule.nama_dokter}
+                          </td>
+                          <td
+                            rowSpan={schedule.jadwal.length}
+                            className="border border-gray-300 px-4 py-2"
+                          >
+                            {schedule.poli}
+                          </td>
+                        </>
+                      )}
+                      <td className="border border-gray-300 px-4 py-2">
+                        {j.hari}
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2">
+                        {j.jam_mulai} - {j.jam_selesai}
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2 capitalize">
+                        {j.sesi}
+                      </td>
+                    </tr>
+                  )),
                 )}
-                <td className="border border-gray-300 px-4 py-2">
-                {jadwal.hari}
-                </td>
-                <td className="border border-gray-300 px-4 py-2">
-                {jadwal.jam_mulai} - {jadwal.jam_selesai}
-                </td>
-                <td className="border border-gray-300 px-4 py-2 capitalize">
-                {jadwal.sesi}
-                </td>
-                </tr>
-                ))} */}
               </Table>
             </section>
           </div>
         </div>
-        <section className="flex flex-col flex-wrap items-center justify-center gap-10 p-6 min-md:flex-row">
+        {/* <section className="flex flex-col flex-wrap justify-center gap-10 p-6 min-md:flex-row">
           {doctors.length > 0 ? (
             doctors.map((doctor, index) => (
               <div className="relative w-min">
@@ -155,7 +166,23 @@ export default function DoctorDetail({ loaderData }: Route.ComponentProps) {
           ) : (
             <p className="text-gray-500">Tidak ada data</p>
           )}
-        </section>
+        </section> */}
+        {doctors.length > 0 && (
+          <section className="p-8">
+            <Slider>
+              {doctors.map((doctor, index) => (
+                <div className="relative mx-2 h-full w-min" key={index}>
+                  <DoctorCard
+                    id={doctor.id_dokter}
+                    name={doctor.nama}
+                    specialty={doctor.poli.nama_poli}
+                    image={doctor.gambar}
+                  />
+                </div>
+              ))}
+            </Slider>
+          </section>
+        )}
       </div>
     </>
   );
